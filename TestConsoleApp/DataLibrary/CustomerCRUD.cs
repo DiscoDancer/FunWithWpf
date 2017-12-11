f88d3024b1d1b05eaf3cb89c23c7b53f8cfbef31
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2017-12-11 22:59:41 +0300

    #14 Сделать таблицы на весь экран
    - customers done

--- TestConsoleApp/DataLibrary/CustomerCRUD.cs
+++ TestConsoleApp/DataLibrary/CustomerCRUD.cs
@@ -7,8 +7,8 @@ namespace DataLibrary
 {
     public static class CustomerCRUD
     {
-        private const string sqlConnect = "Data Source=DESKTOP-37BI6K1;Initial Catalog=InternetShop;User ID=Kreal;Password=2137";
-        //private const string sqlConnect = "Data Source=LAPTOP-VLEV;Initial Catalog=Wpf;User ID=sa;Password=Passw0rd";
+        //private const string sqlConnect = "Data Source=DESKTOP-37BI6K1;Initial Catalog=InternetShop;User ID=Kreal;Password=2137";
+        private const string sqlConnect = "Data Source=LAPTOP-VLEV;Initial Catalog=Wpf;User ID=sa;Password=Passw0rd";
 
         public static List<Customer> GetAll()
         {
--- TestConsoleApp/DataLibrary/EmployeeCRUD.cs
+++ TestConsoleApp/DataLibrary/EmployeeCRUD.cs
@@ -7,7 +7,7 @@ namespace DataLibrary
 {
     public static class EmployeeCRUD
     {
-        private const string sqlConnect = "Data Source=DESKTOP-37BI6K1;Initial Catalog=InternetShop;User ID=Kreal;Password=2137";
+        private const string sqlConnect = "Data Source=LAPTOP-VLEV;Initial Catalog=Wpf;User ID=sa;Password=Passw0rd";
         public static List<Employee> GetAll()
         {
             using (SqlConnection connection = new SqlConnection(sqlConnect))
