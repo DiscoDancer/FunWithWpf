236b8c647e2e9030ff4183449aaec8486058052f
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2017-12-17 15:49:06 +0300

    #19 Удаление работает в WPF
    - пример в WPF

--- TestConsoleApp/WpfApp/Customers.xaml.cs
+++ TestConsoleApp/WpfApp/Customers.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using DataLibrary;
 
@@ -9,10 +10,12 @@ namespace WpfApp
     /// </summary>
     public partial class Customers
     {
+        private List<Customer> _customers = CustomerCRUD.GetAll();
+
         public Customers()
         {
             InitializeComponent();
-            CustomerDataGrid.ItemsSource = CustomerCRUD.GetAll();
+            CustomerDataGrid.ItemsSource = _customers;
         }
 
         private void CustomerDataGrid_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
@@ -24,18 +27,14 @@ namespace WpfApp
         {
 
         }
+
         private void DeleteCustomer(object sender, RoutedEventArgs e)
         {
-            if (MessageBox.Show("Are you sure?", "Deletion", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
-            {
-
-            }
-            else
-            {
-
-            }
-
+            CustomerCRUD.DeleteCustomer(_customers.First());
+            _customers = CustomerCRUD.GetAll();
+            CustomerDataGrid.ItemsSource = _customers;
         }
+
         private void EditCustomer(object sender, RoutedEventArgs e)
         {
             if (MessageBox.Show("Are you sure?", "Editing", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
