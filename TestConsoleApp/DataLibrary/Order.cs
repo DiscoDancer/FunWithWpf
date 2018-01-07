36cc0b47d294ebf0add49c8f275c0c0034e6a7af
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2018-01-07 09:48:41 +0300

    #36 Сделать календарь для заказов
    - фикс binding(а)
    - значение по умолчанию для даты заказа

--- TestConsoleApp/DataLibrary/Order.cs
+++ TestConsoleApp/DataLibrary/Order.cs
@@ -10,7 +10,7 @@ namespace DataLibrary
         public int ProductID { get; set; }
         public int Quantity { get; set; }
         public double Price { get; set; }
-        public DateTime OrderDate { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.Now;
 
         public override string ToString()
         {
